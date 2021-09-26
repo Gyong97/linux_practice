15e918540859b2ddefed64c0379928e3ed278335
Gyong97 <999kgy@naver.com>
2021-09-26 22:03:36 +0900

    add input validation

--- assignment3.sh
+++ assignment3.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 row=$1
 column=$2
+if [ $row -le 1 ]
+then
+echo "first argument must be greater than 0"
+exit 0
+elif [ $column -le 1 ]
+then 
+echo "second argument must be greater than 0"
+exit 0
+fi
 for i in $(seq 1 $row)
 do
 for j in $(seq 1 $column)
